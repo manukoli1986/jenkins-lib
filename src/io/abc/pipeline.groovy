114ca0885184bb34591f4748334bf4cf859c8ff6
Mayank <manukoli1986@gmail.com>
2019-10-27 12:18:10 -0700

    updated src directory

--- src/io/abc/pipeline.groovy
+++ src/io/abc/pipeline.groovy
@@ -1,5 +1,10 @@
 #!/usr/bin/groovy
 package io.abc;
+
+def clone() {
+  git credentialsId: 'GithubCred', url: 'https://github.com/manukoli1986/simple-java-maven-app.git'
+}
+
 def build() {
   mvn clean deploy -U
 }
