599e8c1d5437d1927048a8eaa9d5ba356fd4d5ce
Mayank <manukoli1986@gmail.com>
2019-10-27 13:04:10 -0700

    updated groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,6 +1,6 @@
 def mvn {
-      withMaven(maven: 'maven') {
+//      withMaven(maven: 'maven') {
           sh "mvn clean verify -DSkiptest=True"
-      }
+//      }
 }
 #def mvn(message) {echo 'Hello world'}
