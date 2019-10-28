be30229df73d09c036ec9690dcbf88f8f327a0b0
Mayank <manukoli1986@gmail.com>
2019-10-28 10:10:52 -0700

    updated groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -4,5 +4,5 @@ def call() {
 //      withMaven(maven: 'maven') {
 //      sh 'mvn clean verify -DSkiptest=True'
 //      }
-      /opt/apache-maven-3.5.4/bin/mvn clean verify -DSkiptest=True
+      mvn clean verify -DSkiptest=True
 }
