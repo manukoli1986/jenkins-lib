989b2a6f0b7630c04e21731da4cc21371c90b9f1
Mayank Koli <manukoli1986@gmail.com>
2020-01-26 22:20:30 +0530

    updaed mvn verify

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -8,11 +8,6 @@ tools {
 def call(verify) {
       sh 'mvn clean verify -U -X'
       }
-
-def call(test) {
-      sh 'mvn test -U -X'
-      }
-
 //def call(mvn) {
 //      withMaven(maven: 'maven') {
 //      sh 'mvn clean verify -DSkiptest=True -U -X'
